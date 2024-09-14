25c325ae21d8bf903e4f5291e44d0432f1df9573
flaviusp23 <flavius.paltin@yahoo.com>
2024-09-15 00:27:28 +0300

    cors

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ const port = process.env.PORT || 5000;
 const rateLimiter = require("express-rate-limit");
 const helmet = require("helmet");
 const xss = require("xss-clean");
-const cors = require("cors");
 const mongoSanitize = require("express-mongo-sanitize");
 app.set("trust proxy", 1);
 app.use(
@@ -25,7 +24,6 @@ app.use(
   })
 );
 app.use(helmet());
-app.use(cors());
 app.use(xss());
 app.use(mongoSanitize());
 
